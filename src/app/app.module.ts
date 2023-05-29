65a924d0893b67797b5ef50cf21b694790821d8f
tehkronik <jluzier2@gmail.com>
2023-05-28 23:05:44 -0400

    Added layout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
 import { HistoryComponent } from './header/history.component';
 import { LibraryButtonsComponent } from './header/library-buttons.component';
 import { TopBarComponent } from './header/topbar.component';
 import { UserDropdownComponent } from './header/user-dropdown.component';
+import { LayoutMainComponent } from './layout/layout-main.component';
 import { SidebarButtonsComponent } from './sidebar/sidebar-buttons.component';
 import { SidebarPlaylistComponent } from './sidebar/sidebar-playlist.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -19,7 +21,9 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     UserDropdownComponent,
     SidebarComponent,
     SidebarButtonsComponent,
-    SidebarPlaylistComponent
+    SidebarPlaylistComponent,
+    BodyComponent,
+    LayoutMainComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/body/body.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+import { BaseRouteReuseStrategy } from "@angular/router";
+
+@Component ({
+    selector: "spotify-body",
+    templateUrl: "body.component.html",
+    styleUrls: ["body.component.css"]
+})
+
+export class BodyComponent {
+    
+}
